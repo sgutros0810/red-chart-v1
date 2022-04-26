c3c1e32872f61971c997c291272bb78eb9c665b0
sgutros0810 <sofia54310@gmail.com>
2022-04-26 22:05:05 +0200

    Implemento la librería de selección de codigo de país y verificación del usuario.

--- app/src/main/java/proyecto/red_chart_v1/MainActivity.java
+++ app/src/main/java/proyecto/red_chart_v1/MainActivity.java
@@ -9,35 +9,54 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.hbb20.CountryCodePicker;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonOnSendCode;
     EditText editTextPhome;
+    CountryCodePicker countryCodePicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         buttonOnSendCode = findViewById(R.id.btnSendCode);
         editTextPhome = findViewById(R.id.editTextPhone);
+        countryCodePicker = findViewById(R.id.ccp);
 
         buttonOnSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //goToCodeVerificationActivity();
-                getPhone();
+                getData();
             }
         });
     }
-
-    //Método que captura lo que ha insertado el usuario
-    public void getPhone(){
-        String phone = editTextPhome.getText().toString();
-        Toast.makeText(MainActivity.this, "Teléfono: " + phone,Toast.LENGTH_SHORT).show();
-    }
-
     //Método que nos lleva a la página de verificar código
     public void goToCodeVerificationActivity() {
         Intent intent = new Intent(MainActivity.this, CodeVerificationActivity.class);
         startActivity(intent);
     }
+
+    //Método que captura los datos seleccionados
+    public void getData(){
+        //obtiene el código del pais seleccionado
+        String codeCountry = countryCodePicker.getSelectedCountryCodeWithPlus();
+        //captura el número de teléfono que ha insertado el usuario
+        String phone = editTextPhome.getText().toString();
+
+        //si el telefono está vacio
+       if(phone.equals("")){
+           Toast.makeText(MainActivity.this, "Debe introducir el teléfono.",Toast.LENGTH_SHORT).show();
+       }
+       else {
+           Toast.makeText(MainActivity.this, "Teléfono: " + codeCountry + " " + phone, Toast.LENGTH_SHORT).show();
+       }
+    }
+
+
+
+
 }
