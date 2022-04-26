9bf56c39abdbfc0a9674ae4d9a14204cb7364e32
sgutros0810 <sofia54310@gmail.com>
2022-04-26 19:25:30 +0200

    Captura el texto que ha ingresado el usuario y cambia el tipo de teclado a uno telefónico

--- app/src/main/java/proyecto/red_chart_v1/MainActivity.java
+++ app/src/main/java/proyecto/red_chart_v1/MainActivity.java
@@ -6,23 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonOnSendCode;
+    EditText editTextPhome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonOnSendCode = findViewById(R.id.btnSendCode);
+        editTextPhome = findViewById(R.id.editTextPhone);
 
         buttonOnSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToCodeVerificationActivity();
+                //goToCodeVerificationActivity();
+                getPhone();
             }
         });
     }
+
+    //Método que captura lo que ha insertado el usuario
+    public void getPhone(){
+        String phone = editTextPhome.getText().toString();
+        Toast.makeText(MainActivity.this, "Teléfono: " + phone,Toast.LENGTH_SHORT).show();
+    }
+
     //Método que nos lleva a la página de verificar código
     public void goToCodeVerificationActivity() {
         Intent intent = new Intent(MainActivity.this, CodeVerificationActivity.class);
