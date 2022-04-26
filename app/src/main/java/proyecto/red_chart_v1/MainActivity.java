123eb51d295b678ac4c98e67333d3015fa626ec6
sgutros0810 <sofia54310@gmail.com>
2022-04-26 19:15:28 +0200

    diseño de botón de la pagina principal de registro y modificaciones de la segunda pagina

--- app/src/main/java/proyecto/red_chart_v1/MainActivity.java
+++ app/src/main/java/proyecto/red_chart_v1/MainActivity.java
@@ -2,13 +2,30 @@ package proyecto.red_chart_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonOnSendCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonOnSendCode = findViewById(R.id.btnSendCode);
+
+        buttonOnSendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToCodeVerificationActivity();
+            }
+        });
+    }
+    //Método que nos lleva a la página de verificar código
+    public void goToCodeVerificationActivity() {
+        Intent intent = new Intent(MainActivity.this, CodeVerificationActivity.class);
+        startActivity(intent);
     }
 }
