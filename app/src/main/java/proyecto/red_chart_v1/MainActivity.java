fbfccc6056b5257f3923ec6d906198d3a85da261
sgutros0810 <sofia54310@gmail.com>
2022-04-30 21:07:39 +0200

    Prueba de Firestore

--- app/src/main/java/proyecto/red_chart_v1/MainActivity.java
+++ app/src/main/java/proyecto/red_chart_v1/MainActivity.java
@@ -1,21 +1,32 @@
 package proyecto.red_chart_v1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hbb20.CountryCodePicker;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "GGGG" ;
     Button buttonOnSendCode;
     EditText editTextPhome;
     CountryCodePicker countryCodePicker;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         buttonOnSendCode = findViewById(R.id.btnSendCode);
         editTextPhome = findViewById(R.id.editTextPhone);
         countryCodePicker = findViewById(R.id.ccp);
+        firestore = FirebaseFirestore.getInstance();
+
 
         buttonOnSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +46,32 @@ public class MainActivity extends AppCompatActivity {
                 getData();
             }
         });
+
+        saveData();
     }
+    //Método de prueba
+    private void saveData(){
+        Map<String,Object> map = new HashMap<>();
+        //almacena un nombre
+        map.put("name","Sofia");
+        map.put("nombre","Laura");
+        //firestore.collection("Usuarios").document().set(map);
+        firestore.collection("Usuarios")
+                .add(map)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
     //Método que nos lleva a la página de verificar código
     public void goToCodeVerificationActivity() {
         Intent intent = new Intent(MainActivity.this, CodeVerificationActivity.class);
