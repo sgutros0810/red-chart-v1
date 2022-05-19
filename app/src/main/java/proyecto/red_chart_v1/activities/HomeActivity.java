957cfd6a12b85d9d920a0390c6dcdfe045b66590
sgutros0810 <sofia54310@gmail.com>
2022-05-19 20:32:03 +0200

    Tabs y fragmentos de 'Contactos' y 'Chats'

--- app/src/main/java/proyecto/red_chart_v1/activities/HomeActivity.java
+++ app/src/main/java/proyecto/red_chart_v1/activities/HomeActivity.java
@@ -1,18 +1,30 @@
 package proyecto.red_chart_v1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 import proyecto.red_chart_v1.R;
+import proyecto.red_chart_v1.adapters.ViewPagerAdapter;
+import proyecto.red_chart_v1.fragments.ChatsFragment;
+import proyecto.red_chart_v1.fragments.ContactsFragment;
 import proyecto.red_chart_v1.providers.AuthProvider;
 
 public class HomeActivity extends AppCompatActivity {
     Button mButtonSignOut;
     AuthProvider mAuthProvider;
+
+    TabLayout mTabLayout;
+    ViewPager mViewPager;
+
+    ChatsFragment mChatsFragment;           // Fragmento de Chats
+    ContactsFragment mContactsFragment;     // Fragmento de Contactos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,25 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         mButtonSignOut = findViewById(R.id.btnSignOut);
+        mTabLayout = findViewById(R.id.tabLayout);
+        mViewPager = findViewById(R.id.viewPager);
+
+
+        // --- FRAGMENTOS --- //
+        mViewPager.setOffscreenPageLimit(2);                                                // Contiene un número de fragmentos: Chats y Contactos
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());       // Tiene como parametro un Fragmento
+
+        // Instacia de fragmetos
+        mChatsFragment = new ChatsFragment();
+        mContactsFragment = new ContactsFragment();
+
+        // Añade los fragmentos en el adapter (Fragmento, titulo)
+        adapter.addFragment(mChatsFragment, "CHATS" );
+        adapter.addFragment(mContactsFragment, "CONTACTOS" );
+        mViewPager.setAdapter(adapter);                                                     //Lo añade
+        mTabLayout.setupWithViewPager(mViewPager);
+
+
 
         mAuthProvider = new AuthProvider();
 
---
+++ app/src/main/java/proyecto/red_chart_v1/adapters/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package proyecto.red_chart_v1.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    // SE ENCUENTRA EN LA DOCUMENTACIÓN DE ANDROID STUDIO
+
+    List<Fragment> mFragmentList = new ArrayList<>();
+    List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    // Retorna el tamaño de la lista
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return super.getItemPosition(object);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
