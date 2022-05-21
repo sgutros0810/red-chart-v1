0899a57930869e6b7b5a04606fb8703f3c66191a
sgutros0810 <sofia54310@gmail.com>
2022-05-21 16:52:15 +0200

    Diseño de la pantalla de perfil

---
+++ app/src/main/java/proyecto/red_chart_v1/activities/ProfileActivity.java
@@ -0,0 +1,14 @@
+package proyecto.red_chart_v1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import proyecto.red_chart_v1.R;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
